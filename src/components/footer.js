ecb330bc92c4f411ced5219ed80985d545db48a1
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-08-04 19:27:26 +0530

    🚀 Deploy Final

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedIcon } from '@components/icons';
 import { socialMedia } from '@config';
@@ -44,27 +44,27 @@ const StyledGitHubLink = styled.a`
   color: ${colors.lightSlate};
   padding: 10px;
 `;
-const StyledGitHubInfo = styled.div`
-  margin-top: 10px;
+// const StyledGitHubInfo = styled.div`
+//   margin-top: 10px;
 
-  & > span {
-    display: inline-flex;
-    align-items: center;
-    margin: 0 7px;
-  }
-  svg {
-    display: inline-block;
-    height: 15px;
-    width: auto;
-    margin-right: 5px;
-  }
-`;
+//   & > span {
+//     display: inline-flex;
+//     align-items: center;
+//     margin: 0 7px;
+//   }
+//   svg {
+//     display: inline-block;
+//     height: 15px;
+//     width: auto;
+//     margin-right: 5px;
+//   }
+// `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+  // const [githubInfo, setGitHubInfo] = useState({
+  //   stars: null,
+  //   forks: null,
+  // });
 
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
@@ -72,13 +72,14 @@ const Footer = () => {
     }
     fetch('https://api.github.com/repos/bchiang7/v4')
       .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
+      // .then(json => {
+      //   console.log(json);
+      //   // const { stargazers_count, forks_count } = json;
+      //   // setGitHubInfo({
+      //   //   stars: stargazers_count,
+      //   //   forks: forks_count,
+      //   // });
+      // })
       .catch(e => console.error(e));
   }, []);
 
@@ -115,18 +116,16 @@ const Footer = () => {
             </p>
           </div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <StyledGitHubInfo>
-              <span>
-                <FormattedIcon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <FormattedIcon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </StyledGitHubInfo>
-          )}
+          {/* <StyledGitHubInfo>
+            <span>
+              <FormattedIcon name="Star" />
+              <span>{githubInfo.stars.toLocaleString()}</span>
+            </span>
+            <span>
+              <FormattedIcon name="Fork" />
+              <span>{githubInfo.forks.toLocaleString()}</span>
+            </span>
+          </StyledGitHubInfo> */}
         </StyledGitHubLink>
       </StyledMetadata>
     </StyledContainer>
